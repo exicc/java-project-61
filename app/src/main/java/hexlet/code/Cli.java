554ef5ab68df84fb65c6f6f845c817ebd0e37652
Michael Gavrilov <ping.u@ya.ru>
2023-02-06 16:38:59 +0300

    added checkstyle+config

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-        public static void userInteraction() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("May I have your name? ");
-            String name = scanner.next();
-            System.out.println("Hello, " + name + "!");
-
-            scanner.close();
-        }
+    public static void userInteraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
 }
