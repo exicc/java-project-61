02fecb265476ace07f20cb586a855141d0f1c53c
exicc <ping.u@ya.ru>
2023-02-09 02:05:32 +0300

    step 5 is done

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,30 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.userInteraction();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit""");
+        System.out.print("Your choice: ");
+        String game = scanner.next();
+        switch (game) {
+            case "1":
+                Greeting.userGreeting();
+                break;
+            case "2":
+                Even.isEvenGame();
+                break;
+            case "0":
+                System.out.println("Exit successful");
+                break;
+            default:
+                System.out.println("Error, exiting");
+        }
+        scanner.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,38 @@
+package hexlet.code;
+
+import java.util.Scanner;
+public class Even {
+    public static void isEvenGame() {
+        Scanner scanner = new Scanner(System.in);
+        String userName = Greeting.userGreeting();
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        int rightAnswersCount = 0;
+        for (int i = 0; i < 3; i++) {
+            int ranNum = (int) ((Math.random() * 100) + 1);
+            System.out.println("Question: " + ranNum);
+            System.out.print("Your answer: ");
+            String currentAnswer = scanner.next();
+
+            String rightAnswer;
+            if (ranNum % 2 == 0) {
+                rightAnswer = "yes";
+            } else {
+                rightAnswer = "no";
+            }
+
+            if (currentAnswer.equals(rightAnswer)) {
+                System.out.println("Correct!");
+                rightAnswersCount++;
+            } else {
+                System.out.println("'" + currentAnswer + "' is wrong answer ;(. Correct answer was " + rightAnswer + ".");
+                System.out.println("Let's try again, " + userName + "!");
+                break;
+            }
+        }
+        if (rightAnswersCount >=3) {
+            System.out.println("Congratulations, " + userName + "!");
+        }
+    }
+}
